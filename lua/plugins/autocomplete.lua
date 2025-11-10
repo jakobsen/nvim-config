733189e9817f02bfce01c8281e489721c7f8d364
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2025-11-10 22:17:22 +0100

    Flikk litt på autocomplete
    Sånn at den faktisk funker, f.eks.

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,18 +1,29 @@
 return {
-	'hrsh7th/nvim-cmp',
-	version = false,  -- rarely releases
-	dependencies = {
-		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/cmp-buffer',
-		'hrsh7th/cmp-path',
-	},
-	opts = function()
-		vim.lsp.config("*", { capabilites = require("cmp_nvim_lsp").default_capabilities() })
-		return {
-			auto_brackets = {},
-			completion = {
-				completeopt = "menu.menuone,noinsert"
-			}
-		}
-	end
+    'hrsh7th/nvim-cmp',
+    version = false,
+    dependencies = {
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+    },
+    opts = function()
+        vim.lsp.config("*", { capabilities = require("cmp_nvim_lsp").default_capabilities() })
+        local cmp = require('cmp')
+        return {
+            auto_brackets = {},
+            completion = {
+                completeopt = "menu,menuone,noinsert"
+            },
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'path' },
+            }, {
+                { name = 'buffer' },
+            }),
+            mapping = cmp.mapping.preset.insert({
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-Space>'] = cmp.mapping.complete(),
+            }),
+        }
+    end
 }
